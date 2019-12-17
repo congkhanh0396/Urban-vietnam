3b3eb201df8eec19957aaa8c5116d32db6573e1d
congkhanh0396 <32848812+congkhanh0396@users.noreply.github.com>
2019-12-17 15:03:01 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -1,8 +1,45 @@
-$(document).ready(function()
-{
-    $('.menu-toggle').click(function(){
-        $('nav').toggleClass('active');
-    });
-});
+// $(document).ready(function()
+// {
+//     $('.menu-toggle').click(function(){
+//         $('nav').toggleClass('active');
+//     });
+// });
 
+
+function scrollAppear1(){
+    var aboutEjan = document.querySelector('.intro-ejan-1');
+    var introEjan = aboutEjan.getBoundingClientRect().top;
+    console.log(introEjan);
+    var screenPosition = window.innerHeight / 1.5;
+
+    if(introEjan < screenPosition){
+        aboutEjan.classList.add('about-ejan-appear');
+    }
+}
+
+function scrollAppear2(){
+    var aboutEjan = document.querySelector('.intro-ejan-2');
+    var introEjan = aboutEjan.getBoundingClientRect().top;
+    console.log(introEjan);
+    var screenPosition = window.innerHeight / 1.5;
+
+    if(introEjan < screenPosition){
+        aboutEjan.classList.add('about-ejan-appear');
+    }
+}
+
+function scrollAppear3(){
+    var aboutCompany = document.querySelector('.introCopmany');
+    var introCompany = aboutCompany.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.5;
+    if(introCompany < screenPosition){
+        aboutCompany.classList.add('introCompanyAppear');
+    }
+}
+    
+
+
+window.addEventListener('scroll', scrollAppear1);
+window.addEventListener('scroll', scrollAppear2);
+window.addEventListener('scroll', scrollAppear3);
 
