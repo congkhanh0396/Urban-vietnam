628b7c3cf07cbad3a7721effc16a1cbea0f7ee69
congkhanh0396 <32848812+congkhanh0396@users.noreply.github.com>
2019-12-24 13:17:58 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -5,6 +5,26 @@
 //     });
 // });
 
+var delayInMilliseconds = 2500; 
+
+setTimeout(function loadingCircle() {
+    var loader = document.querySelector('#cover');
+    loader.classList.add('hide');
+    var visible = document.querySelector(".myDiv");
+    visible.classList.add('visible');
+}, delayInMilliseconds);
+
+// function myFunction() {
+//     myVar = setTimeout(showPage, 3000);
+//   }
+  
+//   function showPage() {
+//     var loader = document.querySelector('#cover');
+//       loader.classList.add('hide');
+//     var visible = document.querySelector(".myDiv");
+//     visible.classList.add.apply('visible');
+//   }
+
 
 function scrollAppear1(){
     var aboutEjan = document.querySelector('.intro-ejan-1');
@@ -28,18 +48,20 @@ function scrollAppear2(){
     }
 }
 
-function scrollAppear3(){
-    var aboutCompany = document.querySelector('.introCopmany');
-    var introCompany = aboutCompany.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight / 1.5;
-    if(introCompany < screenPosition){
-        aboutCompany.classList.add('introCompanyAppear');
-    }
-}
+// function scrollAppear3(){
+//     var aboutCompany = document.querySelector('.introCopmany');
+//     var introCompany = aboutCompany.getBoundingClientRect().top;
+//     var screenPosition = window.innerHeight / 1.5;
+//     if(introCompany < screenPosition){
+//         aboutCompany.classList.add('introCompanyAppear');
+//     }
+// }
     
+
+
 
 
 window.addEventListener('scroll', scrollAppear1);
 window.addEventListener('scroll', scrollAppear2);
-window.addEventListener('scroll', scrollAppear3);
+// window.addEventListener('scroll', scrollAppear3);
 
